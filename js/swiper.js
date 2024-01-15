cfdfd3ea0c300d92987988a57be676e76c91f9eb
Sergey Piunov <piunov92@gmail.com>
2024-01-15 21:46:41 +0300

    'init'

---
+++ js/swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+    // mousewheel: {
+    //   sensitivity: 0,
+    // },
+});
