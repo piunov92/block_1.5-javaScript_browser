effa7f625023cdbacab2dd709dc015ebe078df07
Sergey Piunov <piunov92@gmail.com>
2024-01-16 15:53:43 +0300

    fix button

--- js/script.js
+++ js/script.js
@@ -20,3 +20,14 @@ if (middleScreen.matches) {
 window.onresize = function (event) {
     window.setTimeout('location.reload()', 200);
 };
+
+function hideBrand() {
+    let element =  document.querySelectorAll('.hidden');
+    let text = document.querySelector('.show-cards__title').textContent;
+    if (text !== 'Скрыть') {
+        document.querySelector('.show-cards__title').textContent = 'Скрыть';
+    } else {
+        document.querySelector('.show-cards__title').textContent = 'Показать все';
+    }
+    element.forEach(c => c.classList.toggle('cards__item--show'));
+}
